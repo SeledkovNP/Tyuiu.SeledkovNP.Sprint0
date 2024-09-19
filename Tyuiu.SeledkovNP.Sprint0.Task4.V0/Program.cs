2b93569ae59719f6f47bdc313453fec59c08c4df
Tyuiu.SeledkovNP <Tyuiu.SeledkovNP@outlook.com>
2024-09-19 15:50:20 +0500

    Добавлен Task 4

---
+++ Tyuiu.SeledkovNP.Sprint0.Task4.V0/Program.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.SeledkovNP.Sprint0.Task4.V0.Lib;
+
+namespace Tyuiu.SeledkovNP.Sprint0.Task4.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.Addition(1, 5));              //  +
+
+            Console.WriteLine(DataService.Subtraction(1, 5));                 //   -
+
+            Console.WriteLine(DataService.Multiplication(10, 5));                //  *
+
+            Console.WriteLine(DataService.Division(10, 5));              // ( / )
+
+            Console.ReadKey();
+        }
+    }
+}
